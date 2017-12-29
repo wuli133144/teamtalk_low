0ff51652afb3e80feefd052e0c4c2b1dc8e50d68
jackwu <1683358846@qq.com>
2017-12-29 10:42:51 +0800

    测试

---
+++ src/base/CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/TeamTalk/server/src/base/Base64.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/Base64.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/BaseSocket.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/BaseSocket.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/Condition.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/Condition.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/ConfigFileReader.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/ConfigFileReader.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/EncDec.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/EncDec.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/EventDispatch.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/EventDispatch.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/HttpClient.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/HttpClient.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/HttpParserWrapper.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/HttpParserWrapper.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/ImPduBase.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/ImPduBase.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/Lock.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/Lock.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/ServInfo.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/ServInfo.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/Thread.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/Thread.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/ThreadPool.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/ThreadPool.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/TokenValidator.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/TokenValidator.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/UtilPdu.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/UtilPdu.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/http_parser.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/http_parser.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/im_conn_util.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/im_conn_util.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/imconn.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/imconn.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/jsoncpp/json_reader.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_reader.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/jsoncpp/json_value.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_value.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/jsoncpp/json_writer.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_writer.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/log4z.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/log4z.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/netlib.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/netlib.cpp.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.BaseDefine.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.BaseDefine.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Buddy.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Buddy.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.File.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.File.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Group.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Group.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Login.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Login.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Message.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Message.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Other.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Other.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.Server.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Server.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/pb/protocol/IM.SwitchService.pb.cc" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.SwitchService.pb.cc.o"
+  "/mnt/hgfs/TeamTalk/server/src/base/util.cpp" "/mnt/hgfs/TeamTalk/server/src/base/CMakeFiles/base.dir/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  ".."
+  "./jsoncpp"
+  "./pb"
+  "./pb/protocol"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
